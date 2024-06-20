633a17084e01356e71b8d84a4c9c07b9ea35f00f
Sapana Chhetri <chhetrisapana71@gmail.com>
2024-06-20 16:50:02 +0545

    feat: setup httpclient

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,8 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
+import { provideHttpClient, withFetch } from '@angular/common/http';
 
-bootstrapApplication(AppComponent, appConfig)
+// hve removed appconfig from bootstrapapplication method to add httpclient, may need to add appconfig again
+bootstrapApplication(AppComponent, {providers : [provideHttpClient(withFetch())]})
   .catch((err) => console.error(err));
