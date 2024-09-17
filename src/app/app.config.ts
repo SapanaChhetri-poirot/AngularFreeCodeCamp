b15cd97e2e3534e49b4dc878ebb103a9ff447689
Sapana Chhetri <chhetrisapana71@gmail.com>
2024-09-17 08:04:05 +0545

    feat: use of app initializer

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,4 +1,4 @@
-import { ApplicationConfig } from '@angular/core';
+import { APP_INITIALIZER, ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
@@ -6,10 +6,22 @@ import { provideClientHydration } from '@angular/platform-browser';
 import { APP_CONFIG, APP_SERVICE_CONFIG } from './AppConfig/appconfig.service';
 import { HTTP_INTERCEPTORS, HttpClientModule, provideHttpClient, withInterceptors, withInterceptorsFromDi } from '@angular/common/http';
 import { requestInterceptor } from './request.interceptor';
+import { InitService } from './init.service';
+
+function initFactory(initService: InitService){
+  return () => initService.init();
+}
+
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
      provideClientHydration(),
+     {
+        provide: APP_INITIALIZER,
+        useFactory: initFactory,
+        deps: [InitService],
+        multi: true,
+     },
       provideHttpClient(), {provide: APP_SERVICE_CONFIG, useValue: APP_CONFIG},
       // provideHttpClient(withInterceptorsFromDi()), 
       // {
