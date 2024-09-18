58b2f1fd308575f5e0c0c2d7420252b88b09c088
Sapana Chhetri <chhetrisapana71@gmail.com>
2024-09-18 08:31:24 +0545

    feat: implement wild card route for when page is not found

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,11 +2,13 @@ import { Routes } from '@angular/router';
 import { EmployeeComponent } from './employee/employee.component';
 import { RoomsComponent } from './rooms/rooms.component';
 import { ContainerComponent } from './container/container.component';
+import { NotfoundComponent } from './notfound/notfound.component';
 
 export const routes: Routes = [
     { path: 'employee', component: EmployeeComponent},
     { path: 'rooms', component: RoomsComponent},
-    //{ path: '', redirectTo:'/rooms', pathMatch:'full' }, // setting default path
+    { path: '', redirectTo:'/rooms', pathMatch:'full' }, // setting default path
+    { path:'**', component: NotfoundComponent} // if a page is not found, then redirect to 404 or sth like that
 ];
 
 // this module is to configure any routes we want to config
