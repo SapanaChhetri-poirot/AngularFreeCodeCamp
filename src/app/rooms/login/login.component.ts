92e2014257946fee5c18ea3c7985f529ad8c7690
Sapana Chhetri <chhetrisapana71@gmail.com>
2024-09-30 08:21:06 +0545

    feat: use of custom validation in template driven form

--- src/app/rooms/login/login.component.ts
+++ src/app/rooms/login/login.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HoverDirective } from '../../hover.directive';
+import { CommonModule } from '@angular/common';
+import { EmailvalidatorDirective } from '../../emailvalidator/emailvalidator.directive';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [FormsModule, HoverDirective],
+  imports: [FormsModule, HoverDirective, CommonModule, EmailvalidatorDirective],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
